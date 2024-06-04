2ecc5abd5684663211c47568b583b69c2cca3390
栗田駿 <shun3533@softbank.ne.jp>
2024-06-04 15:25:28 +0900

    first commit

--- src/main/java/com/example/demo/controller/MinusController.java
+++ src/main/java/com/example/demo/controller/MinusController.java
@@ -1,24 +1,31 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@controller
+import com.example.demo.service.MinusService;
 
+
+
+@Controller
 public class MinusController {
-	
-	@autowired
-	private calculationservice calculationservice;
-	
-	@getmapping("minus")
-	public string index() {
-		return "index";
-	}
-	
-	@postmapping("inuscalate")
-	public string calculate(@requestparam("number") int number, Model model) {
-		int result = calculationservice.calculate();
-		model.addAttribute("result", result);
-		return "index";
-	}
 
+    @Autowired
+    private MinusService MinusService; // CalculationServiceを注入します。
+    
+    @GetMapping("minus")
+    public String index() {
+        return "index";
+    }
+    
+    @PostMapping("minuscalculate")
+    public String calculate(@RequestParam("number") int number, Model model) {
+        int result = MinusService.calculate(number); // CalculationServiceのメソッドを呼び出します。
+        model.addAttribute("result", result);
+        return "index";
+    }
 }
--- src/main/java/com/example/demo/service/MinusService.java
+++ src/main/java/com/example/demo/service/MinusService.java
@@ -4,9 +4,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MinusService {
-	
-	public int calculate(int number) {
-		return number * 2;
-	}
 
+    public int calculate(int number) {
+        return number - 2; // 入力された数字から2を引く処理
+    }
 }
